ee66f6b46e6e29f57e1442b84ce812cd0cf2c094
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-23 20:46:31 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -9,9 +9,9 @@ from dragonbot.helpers.functions import deEmojify
 @bot.on(admin_cmd(pattern="anime(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="anime(?: |$)(.*)", allow_sudo=True))
 async def nope(cobra):
-    mafia = h1m4n5hu0p.pattern_match.group(1)
+    mafia = cobra.pattern_match.group(1)
     if not mafia:
-        if h1m4n5hu0p.is_reply:
+        if cobra.is_reply:
             (await cobra.get_reply_message()).message
         else:
             await edit_or_reply(cobra, "`Sir please give some query to search and download it for you..!`"
