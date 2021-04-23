f277363d1f7ad6d669faaf8e332eba0843cac5c5
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-23 20:43:46 +0530

    Update anime.py

--- userbot/plugins/anime.py
+++ userbot/plugins/anime.py
@@ -8,25 +8,25 @@ from mafiabot.helpers.functions import deEmojify
 
 @bot.on(admin_cmd(pattern="anime(?: |$)(.*)"))
 @bot.on(sudo_cmd(pattern="anime(?: |$)(.*)", allow_sudo=True))
-async def nope(h1m4n5hu0p):
+async def nope(cobra):
     mafia = h1m4n5hu0p.pattern_match.group(1)
     if not mafia:
         if h1m4n5hu0p.is_reply:
-            (await h1m4n5hu0p.get_reply_message()).message
+            (await cobra.get_reply_message()).message
         else:
-            await edit_or_reply(h1m4n5hu0p, "`Sir please give some query to search and download it for you..!`"
+            await edit_or_reply(cobra, "`Sir please give some query to search and download it for you..!`"
             )
             return
 
     troll = await bot.inline_query("animedb_bot", f"{(deEmojify(mafia))}")
 
     await troll[0].click(
-        h1m4n5hu0p.chat_id,
-        reply_to=h1m4n5hu0p.reply_to_msg_id,
-        silent=True if h1m4n5hu0p.is_reply else False,
+        cobra.chat_id,
+        reply_to=cobra.reply_to_msg_id,
+        silent=True if cobra.is_reply else False,
         hide_via=True,
     )
-    await h1m4n5hu0p.delete()
+    await cobra.delete()
     
 
 CmdHelp("anime").add_command(
