26634e4dbbbac7fb6c5178ba7bd596283342c218
KING-USER1 <82299966+KING-USER1@users.noreply.github.com>
2021-04-24 06:59:44 +0530

    Update arts.py

--- userbot/plugins/arts.py
+++ userbot/plugins/arts.py
@@ -2,7 +2,7 @@ from userbot import ALIVE_NAME, CMD_HELP
 from mafiabot.utils import admin_cmd, edit_or_reply, sudo_cmd
 from userbot.cmdhelp import CmdHelp
 
-DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Mafia User"
+DEFAULTUSER = str(ALIVE_NAME) if ALIVE_NAME else "Dragon User"
 
 USERID = bot.uid
 
